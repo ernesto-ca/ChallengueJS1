5d7eb42b1afec0459751a51aeab7e98e386f12a5
Ernesto Cabañas <71567859+ernesto-ca@users.noreply.github.com>
2021-02-08 17:12:32 -0600

    Simple collider function updated

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
 
-const  GameObject =  {
-    x: 0,
-    y: 0,
-    width: 100,
-    height: 100,
-    collide: function collide(target) {
-        if (this.x < target.x + target.width &&
-            this.x + this.width > target.x &&
-            this.y < target.y + target.height &&
-            this.height + this.y > target.y) {
-             return true;
-         }
-         return false
-    },
-};
-
 const canvas = document.querySelector('.canvas');
 const screen = {
   w: canvas.clientWidth,
@@ -27,9 +11,6 @@ const createWorld = (player_x, player_y, coinsNum) => {
   player.style.top = player_y + "px";
   canvas.appendChild(player);
 
-  player = Object.create(GameObject);
-  player.x = player_x;
-  player.y = player_y;
 
   for (i = 0 ; i < coinsNum; i ++){
     let coin = document.createElement("div");
@@ -37,24 +18,21 @@ const createWorld = (player_x, player_y, coinsNum) => {
     coin.style.left = Math.floor(Math.random() * (screen.w - 60)) + "px";
     coin.style.top = Math.floor(Math.random() * (screen.h - 60)) + "px";
     canvas.appendChild(coin);
-  }
-
-// Colisión a proposito
-    coin = Object.create(GameObject);
-    coin.x = player_x;
-    coin.y = player_y;
-    coin.width = 50;
-    coin.height = 50;
-    console.log(onCollision(player,coin))
-
+    
+    console.log( onCollision(player.getBoundingClientRect(), coin.getBoundingClientRect()));
+   
+    }
+  
 
 }
 createWorld(380, 150, 10);
 
 
-function onCollision(gameobject, target)	{		
-
-    if (gameobject.collide(target)) 			
+function onCollision(player, target)	{		
+    if (player.x < target.x + target.width &&
+        player.x + player.width > target.x &&
+        player.y < target.y + target.height &&
+        player.height + player.y > target.y)		
              return true;		
     return false;	
 }
